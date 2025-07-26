152da2c9d6e99cdae39b67ed070f0e7e3b375f93
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-07-26 15:35:00 +0300

    Refactor prime_utils_test.py

--- rsa_salaus/tests/prime_utils_test.py
+++ rsa_salaus/tests/prime_utils_test.py
@@ -2,191 +2,195 @@ import pytest
 from src.rsa_salaus.prime_utils import sieve_of_eratosthenes, miller_rabin, euclidean, extended_euclidean
 import random
 
-def test_sieve_small():
-    assert sieve_of_eratosthenes(10) == [2, 3, 5, 7]
+class TestSieveOfEratosthenes():
+    def test_sieve_small(self):
+        assert sieve_of_eratosthenes(10) == [2, 3, 5, 7]
 
-def test_sieve_negative():
-    with pytest.raises(ValueError, match="Input must be greater than 1"):
-        sieve_of_eratosthenes(-5)
+    def test_sieve_negative(self):
+        with pytest.raises(ValueError, match="Input must be greater than 1"):
+            sieve_of_eratosthenes(-5)
 
-def test_sieve_zero():
-    with pytest.raises(ValueError, match="Input must be greater than 1"):
-        sieve_of_eratosthenes(0)
+    def test_sieve_zero(self):
+        with pytest.raises(ValueError, match="Input must be greater than 1"):
+            sieve_of_eratosthenes(0)
 
-def test_sieve_one():
-    with pytest.raises(ValueError, match="Input must be greater than 1"):
-        sieve_of_eratosthenes(1)
+    def test_sieve_one(self):
+        with pytest.raises(ValueError, match="Input must be greater than 1"):
+            sieve_of_eratosthenes(1)
 
-def test_sieve_two():
-    assert sieve_of_eratosthenes(2) == [2]
+    def test_sieve_two(self):
+        assert sieve_of_eratosthenes(2) == [2]
 
-def test_sieve_three():
-    assert sieve_of_eratosthenes(3) == [2, 3]
+    def test_sieve_three(self):
+        assert sieve_of_eratosthenes(3) == [2, 3]
 
-def test_sieve_large():
-    assert sieve_of_eratosthenes(200) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
+    def test_sieve_large(self):
+        assert sieve_of_eratosthenes(200) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199]
 
-def test_miller_rabin_small_n():
-    with pytest.raises(ValueError, match="n must be greater than 1"):
-        miller_rabin(1, 40)
+class TestMillerRabin():
+    def test_miller_rabin_small_n(self):
+        with pytest.raises(ValueError, match="n must be greater than 1"):
+            miller_rabin(1, 40)
 
-def test_miller_rabin_2():
-    assert miller_rabin(2, 40) == True
+    def test_miller_rabin_2(self):
+        assert miller_rabin(2, 40) == True
 
-def test_miller_rabin_even_n():
-    numbers = []
-    for i in range(1000, 1100):
-        if i % 2 == 0:
-            numbers.append(i)
-    
-    for number in numbers:
-        assert miller_rabin(number, 40) == False
+    def test_miller_rabin_even_n(self):
+        numbers = []
+        for i in range(1000, 1100):
+            if i % 2 == 0:
+                numbers.append(i)
+        
+        for number in numbers:
+            assert miller_rabin(number, 40) == False
 
-def test_miller_rabin_zero_k():
-    with pytest.raises(ValueError, match="k must be greater than 0"):
-        miller_rabin(3, 0)
+    def test_miller_rabin_zero_k(self):
+        with pytest.raises(ValueError, match="k must be greater than 0"):
+            miller_rabin(3, 0)
 
-def test_miller_rabin_negative_k():
-    with pytest.raises(ValueError, match="k must be greater than 0"):
-        miller_rabin(3, -1)
+    def test_miller_rabin_negative_k(self):
+        with pytest.raises(ValueError, match="k must be greater than 0"):
+            miller_rabin(3, -1)
 
-def test_miller_rabin_5000():
-    numbers = sieve_of_eratosthenes(5000)
-    for number in numbers:
-        assert miller_rabin(number, 40) == True
+    def test_miller_rabin_5000(self):
+        numbers = sieve_of_eratosthenes(5000)
+        for number in numbers:
+            assert miller_rabin(number, 40) == True
 
-def test_miller_rabin_middle():
-    numbers = [7727, 7741, 7753, 7757, 7759, 7789, 7793, 7817, 7823, 7829, 7841, 7853, 7867, 7873, 7877, 7879, 7883, 7901, 7907, 7919]
-    for number in numbers:
-        assert miller_rabin(number, 40) == True
+    def test_miller_rabin_middle(self):
+        numbers = [7727, 7741, 7753, 7757, 7759, 7789, 7793, 7817, 7823, 7829, 7841, 7853, 7867, 7873, 7877, 7879, 7883, 7901, 7907, 7919]
+        for number in numbers:
+            assert miller_rabin(number, 40) == True
 
-def test_miller_rabin_large():
-    numbers = [611693, 611707, 611729, 611753, 611791, 611801, 611803, 611827, 611833, 611837, 611839, 611873, 611879, 611887, 611903, 611921, 611927, 611939, 611951, 611953]
-    for number in numbers:
-        assert miller_rabin(number, 40) == True
+    def test_miller_rabin_large(self):
+        numbers = [611693, 611707, 611729, 611753, 611791, 611801, 611803, 611827, 611833, 611837, 611839, 611873, 611879, 611887, 611903, 611921, 611927, 611939, 611951, 611953]
+        for number in numbers:
+            assert miller_rabin(number, 40) == True
 
-def test_miller_rabin_not_primes():
-    primes = sieve_of_eratosthenes(1224)
+    def test_miller_rabin_not_primes(self):
+        primes = sieve_of_eratosthenes(1224)
 
-    numbers = []
-    for i in range(3, 1223):
-        if i not in primes:
-            numbers.append(i)
+        numbers = []
+        for i in range(3, 1223):
+            if i not in primes:
+                numbers.append(i)
 
-    for number in numbers:
-        assert miller_rabin(number, 40) == False
+        for number in numbers:
+            assert miller_rabin(number, 40) == False
 
-def test_miller_rabin_big_not_primes():
-    primes = [
-    611693, 611707, 611729, 611753, 611791, 611801, 611803, 611827, 611833, 611837,
-    611839, 611873, 611879, 611887, 611903, 611921, 611927, 611939, 611951, 611953
-]
-    
-    non_primes = []
-    for i in range(611693, 611953):
-        if i not in primes:
-            non_primes.append(i)
+    def test_miller_rabin_big_not_primes(self):
+        primes = [
+        611693, 611707, 611729, 611753, 611791, 611801, 611803, 611827, 611833, 611837,
+        611839, 611873, 611879, 611887, 611903, 611921, 611927, 611939, 611951, 611953
+    ]
+        
+        non_primes = []
+        for i in range(611693, 611953):
+            if i not in primes:
+                non_primes.append(i)
 
-    for number in non_primes:
-        assert miller_rabin(number, 40) == False
+        for number in non_primes:
+            assert miller_rabin(number, 40) == False
 
-def test_miller_rabin_2048_primes():
-    primes = [32317006071311007300714876688669951960444102669715484032130345427524655138867890893197201411522913463688717960921898019494119559150490921095088152386448283120630877367300996091750197750389652106796057638384067568276792218642619756161838094338476170470581645852036305042887575891541065808607552399123930385521914333389668342420684974786564569494856176035326322058077805659331026192708460314150258592864177116725943603718461857357598351152301645904403697613233287231227125684710820209725157101726931323469678542580656697935045997268352998638215525166389647960126939249806625440700685819469589938384356951833568218188663, 32317006071311007300714876688669951960444102669715484032130345427524655138867890893197201411522913463688717960921898019494119559150490921095088152386448283120630877367300996091750197750389652106796057638384067568276792218642619756161838094338476170470581645852036305042887575891541065808607552399123930385521914333389668342420684974786564569494856176035326322058077805659331026192708460314150258592864177116725943603718461857357598351152334063994785580370721665417662212881203104945914551140008147396357886767669820042828793708588252247031092071155540224751031064253209884099238184688246467489498721336450133889385773]
+    def test_miller_rabin_2048_primes(self):
+        primes = [32317006071311007300714876688669951960444102669715484032130345427524655138867890893197201411522913463688717960921898019494119559150490921095088152386448283120630877367300996091750197750389652106796057638384067568276792218642619756161838094338476170470581645852036305042887575891541065808607552399123930385521914333389668342420684974786564569494856176035326322058077805659331026192708460314150258592864177116725943603718461857357598351152301645904403697613233287231227125684710820209725157101726931323469678542580656697935045997268352998638215525166389647960126939249806625440700685819469589938384356951833568218188663, 32317006071311007300714876688669951960444102669715484032130345427524655138867890893197201411522913463688717960921898019494119559150490921095088152386448283120630877367300996091750197750389652106796057638384067568276792218642619756161838094338476170470581645852036305042887575891541065808607552399123930385521914333389668342420684974786564569494856176035326322058077805659331026192708460314150258592864177116725943603718461857357598351152334063994785580370721665417662212881203104945914551140008147396357886767669820042828793708588252247031092071155540224751031064253209884099238184688246467489498721336450133889385773]
 
-    for prime in primes:
-        assert miller_rabin(prime, 40) == True
+        for prime in primes:
+            assert miller_rabin(prime, 40) == True
 
-def test_euclidean_small_common_divisor():
-    assert euclidean(48, 18) == 6
+class TestEuclidean():
+    def test_euclidean_small_common_divisor(self):
+        assert euclidean(48, 18) == 6
 
-def test_euclidean_small_coprime():
-    assert euclidean(17, 12) == 1
+    def test_euclidean_small_coprime(self):
+        assert euclidean(17, 12) == 1
 
-def test_euclidean_one():
-    assert euclidean(1, 174562) == 1
+    def test_euclidean_one(self):
+        assert euclidean(1, 174562) == 1
 
-def test_euclidean_large_common_divisor():
-    assert euclidean(10000, 100000) == 10000
+    def test_euclidean_large_common_divisor(self):
+        assert euclidean(10000, 100000) == 10000
 
-def test_euclidean_large_coprime():
-    big_primes = sieve_of_eratosthenes(100000)
+    def test_euclidean_large_coprime(self):
+        big_primes = sieve_of_eratosthenes(100000)
 
-    for i in range(100):
+        for i in range(100):
 
-        if i == 10:
-            a = big_primes[10]
-            b = big_primes[10]
+            if i == 10:
+                a = big_primes[10]
+                b = big_primes[10]
 
-        else:
-            a = random.choice(big_primes)
-            b = random.choice(big_primes)
+            else:
+                a = random.choice(big_primes)
+                b = random.choice(big_primes)
 
-        if a == b:
-            assert euclidean(a, b) == a
-        else:
-            assert euclidean(a, b) == 1
+            if a == b:
+                assert euclidean(a, b) == a
+            else:
+                assert euclidean(a, b) == 1
 
-    
+        
 
-def test_euclidean_samevalue():
-    assert euclidean(2746, 2746) == 2746
+    def test_euclidean_samevalue(self):
+        assert euclidean(2746, 2746) == 2746
 
-def test_euclidean_both_zero():
-    with pytest.raises(ValueError, match="Both values cannot be 0"):
-        euclidean(0, 0)
+    def test_euclidean_both_zero(self):
+        with pytest.raises(ValueError, match="Both values cannot be 0"):
+            euclidean(0, 0)
 
-def test_euclidean_a_zero_b_negative():
-    assert euclidean(0, -173) == 173
+    def test_euclidean_a_zero_b_negative(self):
+        assert euclidean(0, -173) == 173
 
-def test_euclidean_a_negative_b_zero():
-    assert euclidean(-189, 0) == 189
+    def test_euclidean_a_negative_b_zero(self):
+        assert euclidean(-189, 0) == 189
 
-def test_euclidean_both_negative():
-    assert euclidean(-48, -18) == -6
+    def test_euclidean_both_negative(self):
+        assert euclidean(-48, -18) == -6
 
-def test_euclidean_a_negative_b_positive():
-    assert euclidean(-48, 18) == 6
+    def test_euclidean_a_negative_b_positive(self):
+        assert euclidean(-48, 18) == 6
 
-def test_extended_euclidean_common_divisor():
-    x, y = extended_euclidean(30, 12)
-    gcd = euclidean(30, 12)
-    assert 30 * x + 12 * y == gcd
+class TestExtendedEuclidean():
+    def test_extended_euclidean_common_divisor(self):
+        x, y = extended_euclidean(30, 12)
+        gcd = euclidean(30, 12)
+        assert 30 * x + 12 * y == gcd
 
-def test_extended_euclidean_coprime():
-    x, y = extended_euclidean(11, 17)
-    gcd = euclidean(11, 17)
-    assert 11 * x + 17 * y == gcd
+    def test_extended_euclidean_coprime(self):
+        x, y = extended_euclidean(11, 17)
+        gcd = euclidean(11, 17)
+        assert 11 * x + 17 * y == gcd
 
-def test_extended_euclidean_same():
-    x, y = extended_euclidean(11, 11)
-    gcd = euclidean(11, 11)
-    assert 11 * x + 11 * y == gcd
+    def test_extended_euclidean_same(self):
+        x, y = extended_euclidean(11, 11)
+        gcd = euclidean(11, 11)
+        assert 11 * x + 11 * y == gcd
 
-def test_extended_euclidean_a_zero():
-    x, y = extended_euclidean(0, 23)
-    gcd = euclidean(0, 23)
-    assert 0 * x + 23 * y == gcd
+    def test_extended_euclidean_a_zero(self):
+        x, y = extended_euclidean(0, 23)
+        gcd = euclidean(0, 23)
+        assert 0 * x + 23 * y == gcd
 
-def test_extended_euclidean_b_zero():
-    x, y = extended_euclidean(23, 0)
-    gcd = euclidean(23, 0)
-    assert 23 * x + 0 * y == gcd
+    def test_extended_euclidean_b_zero(self):
+        x, y = extended_euclidean(23, 0)
+        gcd = euclidean(23, 0)
+        assert 23 * x + 0 * y == gcd
 
-def test_extended_euclidean_large():
-    x, y = extended_euclidean(5776, 8334)
-    gcd = euclidean(5776, 8334)
-    assert 5776 * x + 8334 * y == gcd
+    def test_extended_euclidean_large(self):
+        x, y = extended_euclidean(5776, 8334)
+        gcd = euclidean(5776, 8334)
+        assert 5776 * x + 8334 * y == gcd
 
-def test_extended_euclidean_one_neg():
-    x, y = extended_euclidean(-23, 19)
-    gcd = euclidean(-23, 19)
-    assert -23 * x + 19 * y == gcd
+    def test_extended_euclidean_one_neg(self):
+        x, y = extended_euclidean(-23, 19)
+        gcd = euclidean(-23, 19)
+        assert -23 * x + 19 * y == gcd
 
-def test_extended_euclidean_both_neg():
-    x, y = extended_euclidean(-23, -19)
-    gcd = euclidean(-23, -19)
-    assert -23 * x + -19 * y == gcd
+    def test_extended_euclidean_both_neg(self):
+        x, y = extended_euclidean(-23, -19)
+        gcd = euclidean(-23, -19)
+        assert -23 * x + -19 * y == gcd
 
-def test_extended_euclidean_both_zero():
-    with pytest.raises(ValueError, match="Both values cannot be 0"):
-        extended_euclidean(0, 0)
+    def test_extended_euclidean_both_zero(self):
+        with pytest.raises(ValueError, match="Both values cannot be 0"):
+            extended_euclidean(0, 0)
