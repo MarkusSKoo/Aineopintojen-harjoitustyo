8fcf1cff70e9014064ca2864864b2ad39233c91a
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-07-27 19:39:00 +0300

    Add time measurement for keypari generation

--- rsa_salaus/src/main.py
+++ rsa_salaus/src/main.py
@@ -1,13 +1,20 @@
+import time
 from rsa_salaus.keygen import generate_keypair
 
 def main():
+    start_time = time.time()
     keypair = generate_keypair()
+    end_time = time.time()
     p = keypair[0]
     q = keypair[1]
 
     print("Kokeillaan avainparin luomista")
     print()
     print(f"p: {p}")
+    print()
+    print(f"q: {q}")
+    print()
+    print(f"Aikaa avainparin luomiseen kului {end_time - start_time} sekuntia")
     
 
 if __name__ == "__main__":
