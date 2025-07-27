b6d86ca2992f2f841411b65bfa597ba6266dd37e
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-07-27 10:46:31 +0300

    Create more unittests

--- rsa_salaus/tests/keygen_test.py
+++ rsa_salaus/tests/keygen_test.py
@@ -15,3 +15,17 @@ from src.rsa_salaus.keygen import (
 def test_generate_1024bit_number():
     number = generate_1024bit_number()
     assert number.bit_length() == 1024
+
+def test_generate_prime():
+    prime = generate_prime()
+    assert prime.bit_length() == 1024
+    assert check_primarility_miller_rabin(prime) is True
+
+def test_generate_keypair():
+    keypair = generate_keypair()
+    p = keypair[0]
+    q = keypair[1]
+    assert p.bit_length() == 1024
+    assert q.bit_length() == 1024
+    assert check_primarility_miller_rabin(p) is True
+    assert check_primarility_miller_rabin(q) is True
