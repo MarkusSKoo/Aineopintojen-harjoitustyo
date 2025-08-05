14361bc58d00c07b8e0da917ae62b00699d37c4d
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-08-05 12:52:06 +0300

    Create more performance tests

--- rsa_salaus/src/main.py
+++ rsa_salaus/src/main.py
@@ -1,7 +1,9 @@
 import time
 from rsa_salaus.keygen import generate_rsa_keys
+from rsa_salaus.rsa_crypt import MessageCryption
 
 def main():
+    crypt = MessageCryption()
     keypair = generate_rsa_keys()
     public_key = keypair[0]
     private_key = keypair[1]
--- rsa_salaus/tests/performance_test.py
+++ rsa_salaus/tests/performance_test.py
@@ -1,13 +1,94 @@
-"""performance_test.py testaa avainparien tuottavan funktion suorituskykyä"""
+"""performance_test.py testaa algoritmien suorituskykyä"""
 
 import time
 import pytest
 from src.rsa_salaus.keygen import generate_rsa_keys
+from src.rsa_salaus.rsa_crypt import MessageCryption
 
-@pytest.mark.performance
-def test_performance_generate_rsa_keys():
-    time_start = time.time()
-    rsa_key = generate_rsa_keys()
-    time_end = time.time()
+class TestPerformance:
+    """Suorituskykyä testaava luokka"""
 
-    assert time_end - time_start <= 4
+    def setup_method(self):
+        """Hakee MessageCryption-luokan ja salausavaimen myöhempiä testejä varten."""
+
+        self.keypair = generate_rsa_keys()
+        self.public_key = self.keypair[0]
+        self.private_key = self.keypair[1]
+
+        self.crypt = MessageCryption()
+
+    @pytest.mark.performance
+    def test_performance_key_generation(self):
+        """Testaa suorituskykyä salausavaimen tuottamisessa."""
+
+        time_start = time.time()
+        generate_rsa_keys()
+        time_end = time.time()
+
+        assert time_end - time_start <= 4
+
+    @pytest.mark.performance
+    def test_performance_message_roundtrip(self):
+        """Testaa viestin salaamisen ja purkamisen yhdistelmän suorituskykyä."""
+
+        time_start = time.time()
+
+        username = "Testuser"
+        plaintext = 'This message is longer and contains special charachters, such as "!#€%&/()=?+_-1234567890'
+
+        encrypted_data = self.crypt.encrypt(username, plaintext, self.public_key)
+        assert username == encrypted_data[0]
+
+        decrypted_data = self.crypt.decrypt(username, encrypted_data[1], self.private_key)
+        assert username == decrypted_data[0]
+        assert plaintext == decrypted_data[1]
+
+        time_end = time.time()
+
+        assert time_end - time_start <= 4
+
+    @pytest.mark.performance
+    def test_performance_encrypt(self):
+        """Testaa suorituskykyä viestin salaamisessa"""
+
+        time_start = time.time()
+        self.crypt.encrypt("Testuser", "message", self.public_key)
+        time_end = time.time()
+
+        assert time_end - time_start <= 1
+
+    @pytest.mark.performance
+    def test_performance_decrypt(self):
+        """Testaa suorituskykyä salatun viestin purkamisessa."""
+
+        username = "Testuser"
+        plaintext = "Hello!"
+
+        encrypted_data = self.crypt.encrypt(username, plaintext, self.public_key)
+
+        time_start = time.time()
+        decrypted_data = self.crypt.decrypt(username, encrypted_data[1], self.private_key)
+        time_end = time.time()
+
+        assert username == decrypted_data[0]
+        assert plaintext == decrypted_data[1]
+        assert time_end - time_start <= 1
+
+    @pytest.mark.performance
+    def test_multiple_roundtrips(self):
+        """Testaa perättäisten roundtrip-pyyntöjen suorituskykyä."""
+
+        username = "Testuser"
+        plaintext = 'This message is longer and contains special charachters, such as "!#€%&/()=?+_-1234567890'
+
+        time_start = time.time()
+
+        for _ in range(100):
+            encrypted_data = self.crypt.encrypt(username, plaintext, self.public_key)
+            decrypted_data = self.crypt.decrypt(username, encrypted_data[1], self.private_key)
+            assert username == decrypted_data[0]
+            assert plaintext == decrypted_data[1]
+
+        time_end = time.time()
+
+        assert time_end - time_start <= 4
