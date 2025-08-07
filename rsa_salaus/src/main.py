bfc2aca1b2e0d1c991ede4aba531deec57339f81
MarkusSKoo <markus.kauhanen@helsinki.fi>
2025-08-07 16:00:10 +0300

    Add error handling and sanity checks

--- rsa_salaus/src/main.py
+++ rsa_salaus/src/main.py
@@ -1,104 +1,129 @@
-import time
+"""main.py-tiedostossa on rajapinta käyttäjälle"""
+
 from rsa_salaus.keygen import generate_rsa_keys
 from rsa_salaus.rsa_crypt import MessageCryption
 
 class UserInterface:
-    """Rajapinta käyttäjälle"""
+    """Rajapinta käyttäjälle komentoriviltä."""
 
     def __init__(self):
-        self.crypt = MessageCryption()
+        self.rsa_cryptor = MessageCryption()
+        self.n_min_size = 2048
 
     def rsa_key(self):
-        """Luo käyttäjälle RSA-salausavainparin"""
+        """Luo käyttäjälle RSA-salausavainparin ja tulostaa sen osat ohjeineen."""
 
         keypair = generate_rsa_keys()
+
         public_n = keypair[0][0]
         public_e = keypair[0][1]
         private_n = keypair[1][0]
         private_d = keypair[1][1]
 
-        print("WARNING: Keep the private key secure at all times! Do not share the private key for security reasons.")
-        print("You can share the public key.")
-        print()
+        print("VAROITUS: Älä jaa yksityistä avainta turvallisuussyistä!")
+        print("Voit jakaa julkisen avaimen.\n")
 
-        print("Your public key first part (N) is:")
-        print()
-        print(public_n)
-        print()
+        print(f"Kopioi ja tallenna RSA-avain. Julkisen avaimen 1. osa (N) on:\n{public_n}\n")
+        print(f"Julkisen avaimen 2. osa (e) on:\n{public_e}\n")
 
-        print("Your public key second part (e) is:")
-        print()
-        print(public_e)
-        print()
-
-        print("Protect your private key! Your private key first part (N) is:")
-        print()
-        print(private_n)
-        print()
-
-        print("Protect your private key! Your private key second part (d) is:")
-        print()
-        print(private_d)
-        print()
+        print("Suojaa yksityinen avaimesi huolellisesti. Voit kopioida sen täältä:\n")
+        print(f"Yksityisen avaimen 1. osa (N) on: \n{private_n}\n")
+        print(f"Yksityisen avaimen 2. osa (d) on: \n{private_d}\n")
 
     def encrypt_message(self):
-        """Salaa käyttäjän syöttämän viestin."""
+        """Salaa käyttäjän syöttämän viestin julkisella avaimella
+        ja tulostaa salatun viestin käyttäjälle."""
 
-        print("You are encrypting a message. Please give your username, message and public key.")
-        print()
+        print("Olet salaamassa viestiä. Anna käyttäjänimi, viesti ja julkinen avain.\n")
 
-        username = input("Give your username: ")
-        message = input("Type your message here: ")
-        print()
+        username = input("Käyttäjänimi: ")
+        message = input("Salattava viesti: ")
 
-        public_key_n = int(input("Give the first part of the public key (N): "))
-        public_key_e = int(input("Give the second part of the public key (e): "))
-        print()
+        try:
+            public_n = int(input("Julkisen avaimen 1. osa (N): "))
+            print()
 
-        public_key = public_key_n, public_key_e
-        encrypted_message = self.crypt.encrypt(username, message, public_key)
+            if public_n.bit_length() < self.n_min_size:
+                print("Liian lyhyt julkinen avain (N)!\n")
+                return
 
-        print(f"Thank you {encrypted_message[0]}, your encrypted message is:")
-        print()
-        print(encrypted_message[1])
-        print()
+            public_e = int(input("Julkisen avaimen 2. osa (e): "))
+            print()
+
+            if public_e <= 0 or public_n <= 0:
+                print("Virhe syötteessä! Avaimen (N ja e) tulee olla positiivisia lukuja.\n")
+                return
+
+        except ValueError:
+            print("Virhe syötteessä! avaimen (N ja e) täytyy olla positiivisia kokonaislukuja.\n")
+            return
+
+        public_key = public_n, public_e
+        encrypted_message = self.rsa_cryptor.encrypt(username, message, public_key)
+
+        print(f"Kiitos {encrypted_message[0]}, kopioi salattu viestisi tästä:\n")
+        print(f"{encrypted_message[1]}\n")
 
     def decrypt_message(self):
-        """Purkaa käyttäjän syöttämän salatun viestin käyttäjän yksityisellä avaimella."""
+        """Purkaa käyttäjän syöttämän salatun viestin yksityisellä avaimella
+        ja tulostaa alkuperäisen viestin."""
 
-        print("You are decrypting a message. Please enter your username, encrypted message and your private key:")
-        print()
+        print("Olet purkamassa salattua viestiä. Anna käyttäjänimi, viesti ja yksityinen avain:\n")
 
-        username = input("Type your username: ")
-        print()
+        username = input("Käyttäjänimi: ")
 
-        message = int(input("Give encrypted message: "))
-        print()
+        try:
+            message = int(input("Purettava viesti: "))
+            print()
+            private_n = int(input("Yksityisen avaimen 1. osa (N): "))
+            print()
 
-        private_n = int(input("Give the first part of the private key (N): "))
-        private_d = int(input("Give the second part of the private key (d): "))
+            if private_n.bit_length() < self.n_min_size:
+                print("Virhe syötteessä: N on liian lyhyt.\n")
+                return
+
+            private_d = int(input("Yksityisen avaimen 2. osa (d): "))
+            print()
+
+        except ValueError:
+            print("Virhe syötteissä! Avaimen (N ja d) ja salatun viestin tulee olla lukuja.\n")
+            return
+
         private_key = private_n, private_d
-        print()
 
-        decrypted_message = self.crypt.decrypt(username, message, private_key)
+        try:
+            decrypted_message = self.rsa_cryptor.decrypt(username, message, private_key)
 
-        print(f"Thank you {decrypted_message[0]}")
-        print()
-        print(f"Your decrypted message is: {decrypted_message[1]}")
-        print()
+        except UnicodeDecodeError:
+            print("Purkaminen epäonnistui! Tarkista että viesti ja avain ovat oikein.\n")
+            return
+
+        print(f"Kiitos {decrypted_message[0]}\n")
+        print(f"Purettu viestisi on: {decrypted_message[1]}\n")
+
+    def show_options(self):
+        """Tulostaa ohjelmassa suoritettavat valinnat."""
+
+        print("Valitse seuraavista vaihtoehdoista:\n")
+
+        print("1. Luo RSA-avain")
+        print("2. Salaa julkisella avaimella")
+        print("3. Pura yksityisellä avaimella")
+        print("4. Lopeta\n")
 
     def main(self):
         """Pääohjelma"""
 
-        print("Welcome to RSA-cryption! Choose from the following:")
-        print()
-        print("1. Generate RSA key")
-        print("2. Encrypt with a public key")
-        print("3. Decrypt with a private key")
-        print("4. Exit")
-        print()
+        print("Tervetuloa RSA-salausohjelmaan!\n")
+
         while True:
-            choice = int(input("Your choice: "))
+            self.show_options()
+
+            try:
+                choice = int(input("Valintasi: "))
+            except ValueError:
+                print("Virheellinen valinta!\n")
+                continue
 
             if choice == 1:
                 self.rsa_key()
@@ -110,13 +135,11 @@ class UserInterface:
                 self.decrypt_message()
 
             elif choice == 4:
-                print("Program closed. Thank you!")
+                print("Ohjelma suljettu. Kiitos!")
                 break
 
             else:
-                print("Invalid command. Restarting.")
-                self.main()
-
+                print("Virheellinen valinta!\n")
 
 if __name__ == "__main__":
     ui = UserInterface()
